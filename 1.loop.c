2f2440d5ac1fcd2356067afc11b2735ea01f4586
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2021-11-03 13:38:55 +0200

    Returns pointer to a newly allocated space in memory

---
+++ 1.loop.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+* _loop - Returns a pointer to a newly allocated space in memory
+*/
+
+void _loop(void)
+{
+		char *input;
+		char **args;
+		int status;
+		int inter = 1, len;
+
+			if (isatty(STDIN_FILENO) != 1)
+				inter = 0;
+
+			signal(SIGINT, sigint_handler);
+
+	do {
+			if (inter != 0)
+{
+			/* Print prompt line. */
+			len = _strlen("$ ");
+			/*printf("#cisfun$ ");*/
+			write(STDOUT_FILENO, "$ ", len);
+}
+			/*Read from prompt line (input) */
+			input = _read_line();
+			if (input == NULL)
+			return;
+
+			/* Split read input into arguments. */
+			args = _split_line(input);
+			if (args == NULL)
+{
+			free(input);
+			return;
+}
+
+			/* Execute according to arguments. */
+			status = _execute(args, input);
+
+			/* Free input and arguments. */
+			free(input);
+			free(args);
+
+	} while (status == 1);
+}
